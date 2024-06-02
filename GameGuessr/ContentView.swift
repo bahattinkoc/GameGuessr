4a22a4ccf51b26efc8f81181a8b2711386481349
BAHATTIN KOC <bahattin.koc@atmosware.turkcell.com.tr>
2024-06-02 18:27:15 +0300

    - Created project

---
+++ GameGuessr/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  GameGuessr
+//
+//  Created by BAHATTIN KOC on 2.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Game Guessr")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameGuessr/GameGuessrApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameGuessrApp.swift
+//  GameGuessr
+//
+//  Created by BAHATTIN KOC on 2.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameGuessrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
